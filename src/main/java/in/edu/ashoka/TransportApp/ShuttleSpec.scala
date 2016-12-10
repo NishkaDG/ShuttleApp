07d36e7866b6d632ba25ca53351dfb3d11c9dbc0
Nishka <nishka.dasgupta@yahoo.com>
2016-12-11 02:59:47 +0530

    Bug fixes

--- src/main/java/in/edu/ashoka/TransportApp/ShuttleSpec.scala
+++ src/main/java/in/edu/ashoka/TransportApp/ShuttleSpec.scala
@@ -2,8 +2,8 @@ import org.scalatest._
 
 class ShuttleSpec extends FlatSpec {
 	"A Shuttle" should "store bookings" in{
-		val testShuttle1 = new Shuttle("13-12-2016 09:00", "Campus", 12)
-		var rv = new Int
+		val testShuttle1: Shuttle = Shuttle("13-12-2016 09:00", "Campus", 12)
+		var rv: Int = 0
 		for(var i <- 1 to 12){
 			assert(testShuttle1.bookSeat("A")==1)
 		}
@@ -14,16 +14,16 @@ class ShuttleSpec extends FlatSpec {
 		
 	}
 	it should "check booking of seats" in {
-		val testShuttle2 = new Shuttle("14-12-2016 10:00", "Campus", 12)
-		var rv = testShuttle2.bookSeat("A")
+		val testShuttle2: Shuttle = Shuttle("14-12-2016 10:00", "Campus", 12)
+		var rv: Int = testShuttle2.bookSeat("A")
 		assert(testShuttle2.checkForBooking("A")==1)
 		assert(testShuttle2.checkForBooking("B")==0)
 	}
 	it should "be equal to another object with the same time and destination" in {
-		val testShuttle3 = new Shuttle("12-12-2016 09:00", "Campus", 12)
-		val testShuttle4 = new Shuttle("12-12-2016 09:00", "Campus", 12)
-		val testShuttle5 = new Shuttle("12-12-2016 09:00", "Jahangirpuri", 12)
-		val testShuttle6 = new Shuttle("12-12-2016 10:00", "Campus", 12)
+		val testShuttle3: Shuttle = Shuttle("12-12-2016 09:00", "Campus", 12)
+		val testShuttle4: Shuttle = Shuttle("12-12-2016 09:00", "Campus", 12)
+		val testShuttle5: Shuttle = Shuttle("12-12-2016 09:00", "Jahangirpuri", 12)
+		val testShuttle6: Shuttle = Shuttle("12-12-2016 10:00", "Campus", 12)
 		assert(testShuttle3.equals(testShuttle4)==True)
 		assert(testShuttle3.equals(testShuttle5)==False)
 		assert(testShuttle3.equals(testShuttle6)==False)
